e0a5c2475902888dad7ea5014e91bf5d14e9b715
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-25 13:19:43 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import {Header} from './components/Header';
+import { Watchlist } from './components/Watchlist';
+import { Watched } from './components/Watched';
+import './lib/font-awesome/css/all.min.css'
+import { Add } from './components/Add';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Router>
+      <Header />
+
+      <Switch>
+        <Route exact path='/'>
+          <Watchlist />
+        </Route>
+        <Route path="/watched">
+          <Watched />
+        </Route>
+        <Route path="/add">
+          <Add />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/context/GlobalState.js
@@ -0,0 +1,8 @@
+import React, {createContext, useReducer, useEffect} from 'react';
+
+//initial state
+const initialState = {
+    watchlist: [],
+    watched: []
+};
+
