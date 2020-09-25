b1d331d90ababd3cc56ce06d1675062caa1dad13
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-09-25 18:21:59 +0530

    rider provider

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,5 @@
 import React, {createContext, useReducer, useEffect} from 'react';
+import {AppReducer} from './AppReducer';
 
 //initial state
 const initialState = {
@@ -6,3 +7,22 @@ const initialState = {
     watched: []
 };
 
+//create context
+export const GlobalContext = createContext(initialState);
+
+//provider components
+export const GlobalProvider = props =>{
+    const [state, dispatch] = useReducer(AppReducer , initialState);
+
+    //actions
+    const addMovieToWatchlist = movie =>{
+        dispatch({type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie})
+    }
+
+
+    return (
+        <GlobalContext.Provider value={{watchlist: state.watchlist, watched: state.watched}}>
+            {props.children}
+        </GlobalContext.Provider>
+    )
+}
